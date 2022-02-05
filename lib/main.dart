719e6712f374c4847f20a34cbbde2aa45b09afc1
paulsump <sumpner@hotmail.com>
2022-02-05 15:35:48 +0000

    added folders.  profiles at 60fps on iphone using profiling tools

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:cube_painter/background.dart';
+import 'package:cube_painter/widgets/background.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(createApp());
