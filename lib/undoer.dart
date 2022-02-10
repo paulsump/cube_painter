8c5ef51f9b667ce43a6b8990824ae9a99234c3b9
paulsump <sumpner@hotmail.com>
2022-02-10 09:30:10 +0000

    tidy

--- lib/undoer.dart
+++ lib/undoer.dart
@@ -15,37 +15,24 @@ class Undoer {
   void save() {
     _saveTo(_undos);
 
-    // out(str(_undos));
     _redos.clear();
   }
 
   void undo(setState) {
     _popFromPushTo(_undos, _redos);
-    // out('${str(_undos)}, ${str(_redos)}->${simpleCubes.length}');
     setState(() {});
   }
 
   void redo(setState) {
     _popFromPushTo(_redos, _undos);
-    // out('${str(_undos)}, ${str(_redos)}->${simpleCubes.length}');
     setState(() {});
   }
 
-  // String str(List list) {
-  //   final lengths = <int>[];
-  //
-  //   for (final item in list) {
-  //     lengths.add(item.length);
-  //   }
-  //   return '${list.length}($lengths)';
-  // }
-
   void _popFromPushTo(DoList popFrom, DoList pushTo) {
     _saveTo(pushTo);
-    final List<CubeInfo> cubeInfos = popFrom.removeLast();
 
-    // _animCubes.clear();
     simpleCubes.clear();
+    final List<CubeInfo> cubeInfos = popFrom.removeLast();
 
     for (final CubeInfo cubeInfo in cubeInfos) {
       simpleCubes.add(SimpleCube(key: UniqueKey(), info: cubeInfo));
