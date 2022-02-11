7f650f9b127e73137858b674038a10b2c1383a5c
paulsump <sumpner@hotmail.com>
2022-02-11 16:50:23 +0000

    extra hexagon+ two files made on phone

--- lib/painter_page.dart
+++ lib/painter_page.dart
@@ -131,7 +131,7 @@ class _PainterPageState extends State<PainterPage> {
             center: Offset(x * (i + 4.5), y),
             radius: radius,
           ),
-        for (int i = 0; i < 8; ++i)
+        for (int i = 0; i < 9; ++i)
           Hexagon(
               center: Offset(x * i, y + 3 * radius * H),
               radius: radius,
