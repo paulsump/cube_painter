e0a402f2c8650faf3f6767374562917ce74214d4
paulsump <sumpner@hotmail.com>
2022-02-13 17:33:50 +0000

    green tiles back

--- lib/colors.dart
+++ lib/colors.dart
@@ -15,21 +15,22 @@ Color getColor(Side side) {
 Color getTweenBLtoTColor(double t) =>
     Color.lerp(getColor(Side.bl), getColor(Side.t), t)!;
 
-Color get buttonColor => getTweenBLtoTColor(0.7);
+// Color get buttonColor => getTweenBLtoTColor(0.7);
+// Color get backgroundColor => getTweenBLtoTColor(0.4);
+Color get buttonColor => getTweenBtoGColor(0.7);
 
-Color get backgroundColor => getTweenBLtoTColor(0.4);
-// Color get buttonColor => getTweenBtoGColor(0.7);
-// Color get backgroundColor => getTweenBtoGColor(0.5);
+Color get backgroundColor => getTweenBtoGColor(0.5);
 
 Color getButtonColor(double t) => Color.lerp(backgroundColor, buttonColor, t)!;
 
-Color get radioButtonOnColor => getButtonColor(0.3);
-// getTweenBtoGColor(0.3);
+Color get radioButtonOnColor =>
+    // getButtonColor(0.3);
+    getTweenBtoGColor(0.3);
 
-Color get radioButtonOffColor => //getButtonColor(0.3);
-    Color.lerp(getColor(Side.t), getColor(Side.br), 0.3)!;
+Color get radioButtonOffColor => getButtonColor(0.3);
+// Color.lerp(getColor(Side.t), getColor(Side.br), 0.3)!;
 
-// const green = Color(0xFF284CA1);
-// const blue = Color(0xFF32B875);
+const green = Color(0xFF284CA1);
+const blue = Color(0xFF32B875);
 
-// Color getTweenBtoGColor(double t) => Color.lerp(blue, green, t)!;
+Color getTweenBtoGColor(double t) => Color.lerp(blue, green, t)!;
