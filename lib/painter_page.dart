52a659bb640bfab30ce8eb64ee7e29d937415dcd
paulsump <sumpner@hotmail.com>
2022-02-13 23:11:56 +0000

    grid fits to any screen (slightly inaccurately atm)

--- lib/painter_page.dart
+++ lib/painter_page.dart
@@ -193,23 +193,25 @@ class _PainterPageState extends State<PainterPage> {
 
     final screen = getScreen(context, listen: false);
     final double scale = getZoomScale(context);
+    final Offset panOffset = getPanOffset(context, listen: false) / scale;
+    final Offset center = screen.center / scale;
 
     final double NX = screen.width / scale;
     final int nx = NX.ceil() + 3;
 
     final double NY = screen.height / scale;
     final int ny = NY.ceil();
-    for (double x = 0; x < screen.width; x += root3over2 * scale) {
-      for (double y = 0; y < screen.height; y += scale) {
-        double h = x % (root3over2 * scale) == 0 ? H * scale : 2 * H * scale;
+    for (int x = 0; x < nx; ++x) {
+      for (int y = 0; y < ny; ++y) {
+        double h = x % 2 == 0 ? 0 : H;
         double Y = h + y.toDouble();
+        double X = W * x.toDouble();
+        X -= panOffset.dx - panOffset.dx % W;
+        Y -= panOffset.dy - panOffset.dy % H;
+        X -= center.dx - center.dx % W;
+        Y -= center.dy - center.dy % H;
         _tiles.add(
-          SimpleTile(
-            bottom: screenToUnit(
-              Offset(W * x, Y),
-              context,
-            ),
-          ),
+          SimpleTile(bottom: Offset(X, Y)),
         );
       }
     }
