7ad0221aa85e1100813e64f048d5c073ce3d2f84
paulsump <sumpner@hotmail.com>
2022-02-13 21:05:17 +0000

    tidy tile

--- lib/cubes/unit_tile.dart
+++ lib/cubes/unit_tile.dart
@@ -1,4 +1,3 @@
-import 'package:cube_painter/colors.dart';
 import 'package:cube_painter/cubes/cube_sides.dart';
 import 'package:cube_painter/out.dart';
 import 'package:flutter/material.dart';
@@ -6,47 +5,23 @@ import 'package:flutter/material.dart';
 const noWarn = out;
 
 class UnitTile extends StatelessWidget {
-  // final double t;
-
   const UnitTile({
     Key? key,
-    // required this.t,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) => const CustomPaint(
-        painter: _Painter(
-            // t: t,
-            // cubeSide: getCubeSides(Crop.c)[1],
-            ),
+        painter: _Painter(),
       );
 }
 
 class _Painter extends CustomPainter {
-  // final CubeSide cubeSide;
-  // final double t;
 
   const _Painter();
 
-  // const _Painter({
-  // required this.t,
-  // required this.cubeSide,
-  // });
-
   @override
   void paint(Canvas canvas, Size size) {
-    // out('lks');
     canvas.drawPath(Path()..addPolygon(topSide, true), paintCache);
-    // Path()..addPolygon(topSide, true), paintCache);
-    // cubeSide.path,
-    // _getGradientPaint(0.5, cubeSide.path, PaintingStyle.fill));
-    // _getGradientPaint(t, cubeSide.path, PaintingStyle.fill));
-
-    // canvas.drawPath(
-    //     cubeSide.path,
-    //     Paint()
-    //       ..color = getColor(cubeSide.side)
-    //       ..style = PaintingStyle.stroke);
   }
 
   @override
@@ -61,17 +36,17 @@ final Paint paintCache = Paint()
   ).createShader(const Rect.fromLTRB(-0.9, -1.0, 0.9, 0.0))
   ..style = PaintingStyle.fill;
 
-const double dt = 0.1;
+// const double dt = 0.1;
 
-Paint _getGradientPaint(double t, Path path, PaintingStyle style) {
-  return Paint()
-    ..shader = _getGradient(t).createShader(path.getBounds())
-    ..style = style;
-}
-
-LinearGradient _getGradient(double t) => LinearGradient(
-      // colors: [getTweenBLtoTColor(t - dt), getTweenBLtoTColor(t + dt)],
-      colors: [getTweenBtoGColor(t - dt), getTweenBtoGColor(t + dt)],
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter,
-    );
+// Paint _getGradientPaint(double t, Path path, PaintingStyle style) {
+//   return Paint()
+//     ..shader = _getGradient(t).createShader(path.getBounds())
+//     ..style = style;
+// }
+//
+// LinearGradient _getGradient(double t) => LinearGradient(
+//       // colors: [getTweenBLtoTColor(t - dt), getTweenBLtoTColor(t + dt)],
+//       colors: [getTweenBtoGColor(t - dt), getTweenBtoGColor(t + dt)],
+//       begin: Alignment.topCenter,
+//       end: Alignment.bottomCenter,
+//     );
