79cb8d25d379be54edbcc8d817cfb00e5e22157b
paulsump <sumpner@hotmail.com>
2022-02-13 18:38:17 +0000

    cacheing paint is no diff from constant params as expected.

--- lib/cubes/unit_tile.dart
+++ lib/cubes/unit_tile.dart
@@ -1,7 +1,10 @@
 import 'package:cube_painter/colors.dart';
 import 'package:cube_painter/cubes/cube_sides.dart';
 import 'package:cube_painter/data/crop.dart';
+import 'package:cube_painter/out.dart';
 import 'package:flutter/material.dart';
+
+const noWarn = out;
 
 class UnitTile extends StatelessWidget {
   final double t;
@@ -31,8 +34,16 @@ class _Painter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    // out( [getTweenBtoGColor(0.5 - dt), getTweenBtoGColor(0.5 + dt)],);
+    // [Color(0xff2e8c86), Color(0xff2c778f)]
+    // out(cubeSide.path.getBounds());
+    // Rect.fromLTRB(-0.9, -1.0, 0.9, 0.0)
     canvas.drawPath(
-        cubeSide.path, _getGradientPaint(t, cubeSide.path, PaintingStyle.fill));
+        // cubeSide.path, cubeSide.getPaint( PaintingStyle.fill));
+        // cubeSide.path,paintCache);
+        cubeSide.path,
+        _getGradientPaint(0.5, cubeSide.path, PaintingStyle.fill));
+    // cubeSide.path, _getGradientPaint(t, cubeSide.path, PaintingStyle.fill));
 
     // canvas.drawPath(
     //     cubeSide.path,
@@ -46,6 +57,14 @@ class _Painter extends CustomPainter {
 }
 
 const double dt = 0.1;
+
+final Paint paintCache = Paint()
+  ..shader = const LinearGradient(
+    colors: [Color(0xff2e8c86), Color(0xff2c778f)],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  ).createShader(const Rect.fromLTRB(-0.9, -1.0, 0.9, 0.0))
+  ..style = PaintingStyle.fill;
 
 Paint _getGradientPaint(double t, Path path, PaintingStyle style) {
   return Paint()
