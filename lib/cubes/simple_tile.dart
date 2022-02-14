9a7a04476c4b5de5b7f48e31c2a1565751a853b0
paulsump <sumpner@hotmail.com>
2022-02-14 19:11:04 +0000

    Moved unit tile to file where it's used

--- lib/cubes/simple_tile.dart
+++ lib/cubes/simple_tile.dart
@@ -1,4 +1,4 @@
-import 'package:cube_painter/cubes/unit_tile.dart';
+import 'package:cube_painter/cubes/cube_sides.dart';
 import 'package:cube_painter/out.dart';
 import 'package:flutter/material.dart';
 
@@ -21,8 +21,54 @@ class SimpleTile extends StatelessWidget {
       offset: bottom,
       child: Transform.scale(
         scale: scale,
-        child: const UnitTile(),
+        child: const _UnitTile(),
       ),
     );
   }
 }
+
+class _UnitTile extends StatelessWidget {
+  const _UnitTile({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => const CustomPaint(
+        painter: _Painter(),
+      );
+}
+
+class _Painter extends CustomPainter {
+  const _Painter();
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawPath(Path()..addPolygon(topSide, true), _paintCache);
+  }
+
+  @override
+  bool shouldRepaint(_Painter oldDelegate) => false;
+}
+
+final Paint _paintCache = Paint()
+  ..shader = const LinearGradient(
+    colors: [Color(0xff2e8c86), Color(0xff2c778f)],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  ).createShader(const Rect.fromLTRB(-0.9, -1.0, 0.9, 0.0))
+  ..style = PaintingStyle.fill;
+
+// const double dt = 0.1;
+
+// Paint _getGradientPaint(double t, Path path, PaintingStyle style) {
+//   return Paint()
+//     ..shader = _getGradient(t).createShader(path.getBounds())
+//     ..style = style;
+// }
+//
+// LinearGradient _getGradient(double t) => LinearGradient(
+//       // colors: [getTweenBLtoTColor(t - dt), getTweenBLtoTColor(t + dt)],
+//       colors: [getTweenBtoGColor(t - dt), getTweenBtoGColor(t + dt)],
+//       begin: Alignment.topCenter,
+//       end: Alignment.bottomCenter,
+//     );
