d60a49c7d80dec21527159f77670433c798a6c96
paulsump <sumpner@hotmail.com>
2022-02-25 22:04:11 +0000

    fixed missing setGestureMode

--- lib/gesture_mode.dart
+++ lib/gesture_mode.dart
@@ -7,7 +7,12 @@ enum GestureMode { panZoom, addWhole, erase, addSlice }
 GestureMode getGestureMode(BuildContext context, {bool listen = false}) =>
     Provider.of<GestureModeNotifier>(context, listen: listen).gestureMode;
 
-void setGestureMode(GestureMode mode, BuildContext context) {}
+void setGestureMode(GestureMode mode, BuildContext context) {
+  final gestureModeNotifier =
+      Provider.of<GestureModeNotifier>(context, listen: false);
+
+  gestureModeNotifier.setMode(mode);
+}
 
 class GestureModeNotifier extends ChangeNotifier {
   var _gestureMode = GestureMode.addWhole;
