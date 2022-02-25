78d233b388ccafcd260312c3dbd64107925007e1
paulsump <sumpner@hotmail.com>
2022-02-25 19:22:20 +0000

    tidy

--- lib/menu/safe_pad.dart
+++ lib/menu/safe_pad.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
+/// padding for the safe area at the top
 class SafePad extends StatelessWidget {
-  const SafePad({
-    Key? key,
-  }) : super(key: key);
+  const SafePad({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return SizedBox(height: 10.0 + MediaQuery.of(context).padding.top);
-  }
+  Widget build(BuildContext context) =>
+      SizedBox(height: 10.0 + MediaQuery.of(context).padding.top);
 }
