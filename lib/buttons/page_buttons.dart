746e97cbca05d7f67ca05af0f60e74d993aca24c
paulsump <sumpner@hotmail.com>
2022-02-25 23:38:38 +0000

    column of buttons

--- lib/buttons/page_buttons.dart
+++ lib/buttons/page_buttons.dart
@@ -6,7 +6,6 @@ import 'package:cube_painter/colors.dart';
 import 'package:cube_painter/downloaded_icons.dart';
 import 'package:cube_painter/gesture_mode.dart';
 import 'package:cube_painter/out.dart';
-import 'package:cube_painter/transform/screen.dart';
 import 'package:cube_painter/undoer.dart';
 import 'package:flutter/material.dart';
 
@@ -24,68 +23,80 @@ class PageButtons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final gestureMode = getGestureMode(context, listen: true);
-    final double width = getScreenWidth(context);
+    // final double width = getScreenWidth(context);
 
     final bool canUndo = undoer.canUndo;
     final bool canRedo = undoer.canRedo;
 
-    return Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      const OpenFileMenuButton(),
-      SizedBox(
-        height: 72,
-        child: CubeButton(
-          radioOn: GestureMode.addWhole == gestureMode,
-          icon: DownloadedIcons.plusOutline,
-          iconSize: downloadedIconSize,
-          onPressed: () => setGestureMode(GestureMode.addWhole, context),
-          tip:
-              'Tap or drag on the canvas to add a row of cubes. You can change the direction while you drag.',
-        ),
-      ),
-      CubeButton(
-        radioOn: GestureMode.erase == gestureMode,
-        icon: DownloadedIcons.cancelOutline,
-        iconSize: downloadedIconSize,
-        onPressed: () {
-          setGestureMode(GestureMode.erase, context);
-        },
-        tip:
-            'Tap on a cube to delete it.  You can change the position while you have your finger down.',
-      ),
-      HexagonElevatedButton(
-        height: 68,
-        radioOn: GestureMode.panZoom == gestureMode,
-        child: Icon(
-          Icons.zoom_in_sharp,
-          size: iconSize * 1.2,
-          color: enabledIconColor,
-        ),
-        onPressed: () => setGestureMode(GestureMode.panZoom, context),
-        tip: 'Pinch to zoom, drag to move around.',
-      ),
-      const OpenSliceMenuButton(),
-      if (canUndo || canRedo)
-        HexagonElevatedButton(
-          child: Icon(
-            Icons.undo_sharp,
-            size: iconSize,
-            color: canUndo ? enabledIconColor : disabledIconColor,
-          ),
-          onPressed: canUndo ? undoer.undo : null,
-          tip: 'Undo the last add or delete operation.',
-        ),
-      if (canRedo)
-        HexagonElevatedButton(
-          tip: 'Redo the last add or delete operation that was undone.',
-          child: Icon(
-            Icons.redo_sharp,
-            color: canRedo ? enabledIconColor : disabledIconColor,
-            size: iconSize,
-          ),
-          onPressed: canRedo ? undoer.redo : null,
-        ),
-      // HACK without this container, the buttons don't work
-      Container(),
-    ]);
+    return Column(
+      children: [
+        Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const OpenFileMenuButton(),
+              Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                SizedBox(
+                  height: 72,
+                  child: CubeButton(
+                    radioOn: GestureMode.addWhole == gestureMode,
+                    icon: DownloadedIcons.plusOutline,
+                    iconSize: downloadedIconSize,
+                    onPressed: () =>
+                        setGestureMode(GestureMode.addWhole, context),
+                    tip:
+                        'Tap or drag on the canvas to add a row of cubes. You can change the direction while you drag.',
+                  ),
+                ),
+                CubeButton(
+                  radioOn: GestureMode.erase == gestureMode,
+                  icon: DownloadedIcons.cancelOutline,
+                  iconSize: downloadedIconSize,
+                  onPressed: () {
+                    setGestureMode(GestureMode.erase, context);
+                  },
+                  tip:
+                      'Tap on a cube to delete it.  You can change the position while you have your finger down.',
+                ),
+                HexagonElevatedButton(
+                  height: 68,
+                  radioOn: GestureMode.panZoom == gestureMode,
+                  child: Icon(
+                    Icons.zoom_in_sharp,
+                    size: iconSize * 1.2,
+                    color: enabledIconColor,
+                  ),
+                  onPressed: () => setGestureMode(GestureMode.panZoom, context),
+                  tip: 'Pinch to zoom, drag to move around.',
+                ),
+                const OpenSliceMenuButton(),
+              ]),
+            ]),
+        Column(children: [
+          if (canUndo || canRedo)
+            HexagonElevatedButton(
+              child: Icon(
+                Icons.undo_sharp,
+                size: iconSize,
+                color: canUndo ? enabledIconColor : disabledIconColor,
+              ),
+              onPressed: canUndo ? undoer.undo : null,
+              tip: 'Undo the last add or delete operation.',
+            ),
+          if (canRedo)
+            HexagonElevatedButton(
+              tip: 'Redo the last add or delete operation that was undone.',
+              child: Icon(
+                Icons.redo_sharp,
+                color: canRedo ? enabledIconColor : disabledIconColor,
+                size: iconSize,
+              ),
+              onPressed: canRedo ? undoer.redo : null,
+            ),
+          // HACK without this container, the buttons don't work
+          Container(),
+        ]),
+      ],
+    );
   }
 }
