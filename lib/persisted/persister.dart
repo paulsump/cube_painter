84236fde15421a8084903f6e84a742b4bd1ad763
paulsump <sumpner@hotmail.com>
2022-03-02 21:01:37 +0000

    extracted everything from SketchBank that doesn't have a notifyListeners() into a mixin called Persistor.

---
+++ lib/persisted/persister.dart
@@ -0,0 +1,239 @@
+import 'dart:async';
+import 'dart:collection';
+import 'dart:io';
+
+import 'package:cube_painter/out.dart';
+import 'package:cube_painter/persisted/assets.dart';
+import 'package:cube_painter/persisted/cube_info.dart';
+import 'package:cube_painter/persisted/persist.dart';
+import 'package:cube_painter/persisted/settings.dart';
+import 'package:cube_painter/persisted/sketch.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+const noWarn = out;
+
+/// access to the main store of the entire model
+/// For loading and saving all the cube positions and their info
+/// loaded from a json file.
+/// Also manages the starting and stopping of cube animation
+/// during loading and brushing.
+/// init() is the main starting point for the app.
+/// TODO MOVe anim stuff into Animator
+/// todo move load stuff into Persister
+mixin Persister {
+  final sketches = <String, Sketch>{};
+
+  final slicesExample = _SlicesExample();
+
+  String get json => sketch.toString();
+
+  final animCubeInfos = <CubeInfo>[];
+
+  bool isAnimatingLoadedCubes = true;
+
+  bool isBrushing = false;
+
+  void setIsPingPong(bool value) {
+    assert(false); //stub only, want to be calling derived class
+  }
+
+  void updateAfterLoad(BuildContext context) {
+    assert(false); //stub only, want to be callin derived classe
+  }
+
+  void finishAnim() {
+    assert(false); //stub only, want to be callin derived classe
+  }
+
+  /// move all the (static) cubeInfos to animCubeInfos
+  @protected
+  void startAnimatingLoadedCubes() {
+    final List<CubeInfo> cubeInfos = sketch.cubeInfos;
+
+    animCubeInfos.clear();
+    animCubeInfos.addAll(cubeInfos.toList());
+
+    isAnimatingLoadedCubes = true;
+
+    // for correctness and just in case (i saw it ping pong forever one)
+    setIsPingPong(false);
+  }
+
+  bool isPingPong = false;
+
+  final _settingsPersister = SettingsPersister();
+  late Settings _settings;
+
+  String _savedJson = '';
+
+  bool get modified => json != _savedJson;
+
+  bool get hasCubes =>
+      sketches.isNotEmpty &&
+      _hasSketchForCurrentFilePath &&
+      sketch.cubeInfos.isNotEmpty;
+
+  bool get _hasSketchForCurrentFilePath {
+    if (!sketches.containsKey(currentFilePath)) {
+      out(currentFilePath);
+
+      return false;
+    }
+    return true;
+  }
+
+  String get currentFilePath => _settings.currentFilePath;
+
+  void saveCurrentFilePath(String filePath) {
+    _settings.currentFilePath = filePath;
+
+    unawaited(_settingsPersister.save());
+  }
+
+  Sketch get sketch {
+    if (!_hasSketchForCurrentFilePath) {
+      assert(false,
+          "sketches doesn't contain key of currentFilePath: $currentFilePath");
+
+      // prevent irreversible crash for now, for debugging purposes.
+      return Sketch.fromEmpty();
+    }
+    return sketches[currentFilePath]!;
+  }
+
+  void setSketch(Sketch sketch) => sketches[currentFilePath] = sketch;
+
+  UnmodifiableListView<MapEntry> get sketchEntries =>
+      UnmodifiableListView<MapEntry>(sketches.entries.toList());
+
+  /// The main starting point for the app.
+  Future<void> init(BuildContext context) async {
+    _settings = await _settingsPersister.load();
+
+    if (!_settings.copiedSamples) {
+      await copySamples();
+
+      _settings.copiedSamples = true;
+      await _settingsPersister.save();
+    }
+
+    final firstPath = await _loadAllSketches();
+
+    if (currentFilePath.isEmpty) {
+      //  Most recently created is now first in list
+      saveCurrentFilePath(firstPath);
+    }
+
+    if (!sketches.containsKey(currentFilePath)) {
+      out("currentFilePath not found ('$currentFilePath'), so using the first in the list");
+
+      saveCurrentFilePath(firstPath);
+    }
+
+    _savedJson = json;
+    updateAfterLoad(context);
+
+    unawaited(slicesExample.init());
+  }
+
+  // insert at the top of the list
+  void pushSketch(Sketch sketch) {
+    final copy = Map<String, Sketch>.from(sketches);
+
+    sketches.clear();
+    setSketch(sketch);
+
+    sketches.addAll(copy);
+  }
+
+  Future<void> newFile(BuildContext context) async {
+    finishAnim();
+
+    await _setNewFilePath();
+
+    pushSketch(Sketch.fromEmpty());
+    _savedJson = json;
+
+    updateAfterLoad(context);
+    unawaited(saveFile());
+  }
+
+  void loadFile({required String filePath, required BuildContext context}) {
+    saveCurrentFilePath(filePath);
+
+    _savedJson = json;
+    updateAfterLoad(context);
+  }
+
+  Future<void> saveFile() async {
+    finishAnim();
+
+    await saveString(filePath: currentFilePath, string: json);
+    _savedJson = json;
+  }
+
+  Future<void> saveACopyFile() async {
+    finishAnim();
+
+    final jsonCopy = json;
+
+    await _setNewFilePath();
+    pushSketch(Sketch.fromString(jsonCopy));
+
+    _savedJson = json;
+    unawaited(saveFile());
+  }
+
+  void addCubeInfo(CubeInfo info) => sketch.cubeInfos.add(info);
+
+  Future<void> _setNewFilePath() async {
+    final String appFolderPath = await getAppFolderPath();
+
+    final int uniqueId =
+        (DateTime.now().millisecondsSinceEpoch - 1645648060000) ~/ 100;
+
+    saveCurrentFilePath('$appFolderPath$uniqueId$cubesExtension');
+  }
+
+  Future<void> resetCurrentSketch() async =>
+      sketches[currentFilePath] = Sketch.fromString(_savedJson);
+
+  void clear() => sketch.cubeInfos.clear();
+
+  Future<String> _loadAllSketches({bool ignoreCurrent = false}) async {
+    final Directory appFolder = await getApplicationDocumentsDirectory();
+
+    List<String> paths = await getAllAppFilePaths(appFolder);
+
+    // display in reverse chronological order (most recent first)
+    // this is because the file name is a number that increases with time.
+    paths.sort((a, b) => b.compareTo(a));
+
+    for (final String path in paths) {
+      if (!(ignoreCurrent && path == currentFilePath)) {
+        final File file = File(path);
+
+        sketches[path] = Sketch.fromString(await file.readAsString());
+      }
+    }
+
+    return paths.first;
+  }
+}
+
+class _SlicesExample {
+  late UnitTransform unitTransform;
+
+  late Sketch triangleWithGap;
+  late Sketch triangleGap;
+
+  Future<void> init() async {
+    final assets = await Assets.getStrings('help/triangle_');
+
+    triangleWithGap = Sketch.fromString(assets['triangle_with_gap.json']!);
+    triangleGap = Sketch.fromString(assets['triangle_gap.json']!);
+
+    unitTransform = triangleWithGap.unitTransform;
+  }
+}
--- lib/persisted/sketch_bank.dart
+++ lib/persisted/sketch_bank.dart
@@ -1,16 +1,11 @@
 import 'dart:async';
-import 'dart:collection';
 import 'dart:io';
 
 import 'package:cube_painter/out.dart';
-import 'package:cube_painter/persisted/assets.dart';
-import 'package:cube_painter/persisted/cube_info.dart';
-import 'package:cube_painter/persisted/persist.dart';
-import 'package:cube_painter/persisted/settings.dart';
+import 'package:cube_painter/persisted/persister.dart';
 import 'package:cube_painter/persisted/sketch.dart';
 import 'package:cube_painter/undo_notifier.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 
 const noWarn = out;
@@ -25,20 +20,7 @@ SketchBank getSketchBank(BuildContext context, {bool listen = false}) =>
 /// during loading and brushing.
 /// init() is the main starting point for the app.
 /// TODO MOVe anim stuff into Animator
-/// todo move load stuff into Persister
-class SketchBank extends ChangeNotifier {
-  final _sketches = <String, Sketch>{};
-
-  final slicesExample = _SlicesExample();
-
-  String get json => sketch.toString();
-
-  final animCubeInfos = <CubeInfo>[];
-
-  bool isAnimatingLoadedCubes = true;
-
-  bool isBrushing = false;
-
+class SketchBank extends ChangeNotifier with Persister {
   void startBrushing() {
     setIsPingPong(true);
     finishAnim();
@@ -71,159 +53,20 @@ class SketchBank extends ChangeNotifier {
     }
   }
 
-  /// move all the (static) cubeInfos to animCubeInfos
-  void _startAnimatingLoadedCubes() {
-    final List<CubeInfo> cubeInfos = sketch.cubeInfos;
-
-    animCubeInfos.clear();
-    animCubeInfos.addAll(cubeInfos.toList());
-
-    isAnimatingLoadedCubes = true;
-
-    // for correctness and just in case (i saw it ping pong forever one)
-    setIsPingPong(false);
-  }
-
   void setIsPingPong(bool value) {
     isPingPong = value;
     notifyListeners();
   }
 
-  bool isPingPong = false;
-
-  final _settingsPersister = SettingsPersister();
-  late Settings _settings;
-
-  String _savedJson = '';
-
-  bool get modified => json != _savedJson;
-
-  bool get hasCubes =>
-      _sketches.isNotEmpty &&
-      _hasSketchForCurrentFilePath &&
-      sketch.cubeInfos.isNotEmpty;
-
-  bool get _hasSketchForCurrentFilePath {
-    if (!_sketches.containsKey(currentFilePath)) {
-      out(currentFilePath);
-
-      return false;
-    }
-    return true;
-  }
-
-  String get currentFilePath => _settings.currentFilePath;
-
-  void saveCurrentFilePath(String filePath) {
-    _settings.currentFilePath = filePath;
-
-    unawaited(_settingsPersister.save());
-  }
-
-  Sketch get sketch {
-    if (!_hasSketchForCurrentFilePath) {
-      assert(false,
-          "_sketches doesn't contain key of currentFilePath: $currentFilePath");
-
-      // prevent irreversible crash for now, for debugging purposes.
-      return Sketch.fromEmpty();
-    }
-    return _sketches[currentFilePath]!;
-  }
-
-  void setSketch(Sketch sketch) => _sketches[currentFilePath] = sketch;
-
-  UnmodifiableListView<MapEntry> get sketchEntries =>
-      UnmodifiableListView<MapEntry>(_sketches.entries.toList());
-
-
-  /// The main starting point for the app.
-  Future<void> init(BuildContext context) async {
-    _settings = await _settingsPersister.load();
-
-    if (!_settings.copiedSamples) {
-      await copySamples();
-
-      _settings.copiedSamples = true;
-      await _settingsPersister.save();
-    }
-
-    final firstPath = await _loadAllSketches();
-
-    if (currentFilePath.isEmpty) {
-      //  Most recently created is now first in list
-      saveCurrentFilePath(firstPath);
-    }
-
-    if (!_sketches.containsKey(currentFilePath)) {
-      out("currentFilePath not found ('$currentFilePath'), so using the first in the list");
-
-      saveCurrentFilePath(firstPath);
-    }
-
-    _savedJson = json;
-    _updateAfterLoad(context);
-
-    unawaited(slicesExample.init());
-  }
-
-  void _updateAfterLoad(BuildContext context) {
+  void updateAfterLoad(BuildContext context) {
     // TODO if fail, alert user, perhaps skip
     // TODO iff finally:
-    if (_sketches.isNotEmpty) {
+    if (sketches.isNotEmpty) {
       getUndoer(context).clear();
 
-      _startAnimatingLoadedCubes();
+      startAnimatingLoadedCubes();
       notifyListeners();
     }
-  }
-
-  // insert at the top of the list
-  void pushSketch(Sketch sketch) {
-    final copy = Map<String, Sketch>.from(_sketches);
-
-    _sketches.clear();
-    setSketch(sketch);
-
-    _sketches.addAll(copy);
-  }
-
-  Future<void> newFile(BuildContext context) async {
-    finishAnim();
-
-    await _setNewFilePath();
-
-    pushSketch(Sketch.fromEmpty());
-    _savedJson = json;
-
-    _updateAfterLoad(context);
-    unawaited(saveFile());
-  }
-
-  void loadFile({required String filePath, required BuildContext context}) {
-    saveCurrentFilePath(filePath);
-
-    _savedJson = json;
-    _updateAfterLoad(context);
-  }
-
-  Future<void> saveFile() async {
-    finishAnim();
-
-    await saveString(filePath: currentFilePath, string: json);
-    _savedJson = json;
-  }
-
-  Future<void> saveACopyFile() async {
-    finishAnim();
-
-    final jsonCopy = json;
-
-    await _setNewFilePath();
-    pushSketch(Sketch.fromString(jsonCopy));
-
-    _savedJson = json;
-    unawaited(saveFile());
   }
 
   /// Creates a sketch from a json string
@@ -234,22 +77,8 @@ class SketchBank extends ChangeNotifier {
     notifyListeners();
   }
 
-  void addCubeInfo(CubeInfo info) => sketch.cubeInfos.add(info);
-
-  Future<void> _setNewFilePath() async {
-    final String appFolderPath = await getAppFolderPath();
-
-    final int uniqueId =
-        (DateTime.now().millisecondsSinceEpoch - 1645648060000) ~/ 100;
-
-    saveCurrentFilePath('$appFolderPath$uniqueId$cubesExtension');
-  }
-
-  Future<void> resetCurrentSketch() async =>
-      _sketches[currentFilePath] = Sketch.fromString(_savedJson);
-
   Future<void> deleteCurrentFile(BuildContext context) async {
-    _sketches.remove(currentFilePath);
+    sketches.remove(currentFilePath);
 
     final File file = File(currentFilePath);
 
@@ -258,50 +87,12 @@ class SketchBank extends ChangeNotifier {
       file.delete();
     }
 
-    if (_sketches.isEmpty) {
+    if (sketches.isEmpty) {
       await newFile(context);
     } else {
-      loadFile(filePath: _sketches.keys.first, context: context);
+      loadFile(filePath: sketches.keys.first, context: context);
     }
 
     notifyListeners();
-  }
-
-  void clear() => sketch.cubeInfos.clear();
-
-  Future<String> _loadAllSketches({bool ignoreCurrent = false}) async {
-    final Directory appFolder = await getApplicationDocumentsDirectory();
-
-    List<String> paths = await getAllAppFilePaths(appFolder);
-
-    // display in reverse chronological order (most recent first)
-    // this is because the file name is a number that increases with time.
-    paths.sort((a, b) => b.compareTo(a));
-
-    for (final String path in paths) {
-      if (!(ignoreCurrent && path == currentFilePath)) {
-        final File file = File(path);
-
-        _sketches[path] = Sketch.fromString(await file.readAsString());
-      }
-    }
-
-    return paths.first;
-  }
-}
-
-class _SlicesExample {
-  late UnitTransform unitTransform;
-
-  late Sketch triangleWithGap;
-  late Sketch triangleGap;
-
-  Future<void> init() async {
-    final assets = await Assets.getStrings('help/triangle_');
-
-    triangleWithGap = Sketch.fromString(assets['triangle_with_gap.json']!);
-    triangleGap = Sketch.fromString(assets['triangle_gap.json']!);
-
-    unitTransform = triangleWithGap.unitTransform;
   }
 }
