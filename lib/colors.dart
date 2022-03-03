2507028f134db6452b01445d69167811e6c13164
paulsump <sumpner@hotmail.com>
2022-03-03 09:28:53 +0000

    tipColor was too see through on slicemenu

--- lib/colors.dart
+++ lib/colors.dart
@@ -31,7 +31,7 @@ Color get paintingsMenuButtonsColor => _blt.withOpacity(0.7);
 
 Color get alertColor => _blt.withOpacity(0.9);
 
-Color get tipColor => _blt.withOpacity(0.7);
+Color get tipColor => _blt;
 
 Color _getTweenTtoBRColor(double t) =>
     Color.lerp(topColor, bottomRightColor, t)!;
