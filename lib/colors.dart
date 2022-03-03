374331676905f63d7d59b45a4380a041bb6a3b79
paulsump <sumpner@hotmail.com>
2022-03-03 19:15:03 +0000

    color: change all these from getters to finals

--- lib/colors.dart
+++ lib/colors.dart
@@ -12,26 +12,25 @@ Color _getTweenBLtoBRColor(double t) =>
 const Color enabledIconColor = bottomRightColor;
 const Color textColor = enabledIconColor;
 
-// TODO change all these from getters to finals
 Color getTweenBLtoTColor(double t) => Color.lerp(bottomLeftColor, topColor, t)!;
 
 Color getButtonColor(double t) => Color.lerp(backgroundColor, buttonColor, t)!;
 
-Color get buttonColor => _getTweenTtoBRColor(0.4);
+final Color buttonColor = _getTweenTtoBRColor(0.4);
 
-Color get buttonBorderColor => _getTweenTtoBRColor(0.1);
+final Color buttonBorderColor = _getTweenTtoBRColor(0.1);
 
-Color get backgroundColor => _getTweenTtoBRColor(0.6);
+final Color backgroundColor = _getTweenTtoBRColor(0.6);
 
-Color get radioButtonOnColor => _getTweenTtoBRColor(0.4);
+final Color radioButtonOnColor = _getTweenTtoBRColor(0.4);
 
 final Color _blt = getTweenBLtoTColor(0.3);
 
-Color get paintingsMenuButtonsColor => _blt.withOpacity(0.7);
+final Color paintingsMenuButtonsColor = _blt.withOpacity(0.7);
 
-Color get alertColor => _blt.withOpacity(0.9);
+final Color alertColor = _blt.withOpacity(0.9);
 
-Color get tipColor => _blt;
+final Color tipColor = _blt;
 
 Color _getTweenTtoBRColor(double t) =>
     Color.lerp(topColor, bottomRightColor, t)!;
