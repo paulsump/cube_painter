bdcc9f98a50d10d21e466c9b1f8a31673b846be3
paulsump <sumpner@hotmail.com>
2022-03-05 08:54:48 +0000

    rename local reorder functions

--- lib/transform/position_to_unit.dart
+++ lib/transform/position_to_unit.dart
@@ -3,19 +3,18 @@ import 'package:flutter/material.dart';
 
 const root3over2 = 0.8660254037844386; // sqrt(3)/2
 
-const double W = root3over2;
-const double H = 0.5;
+const double _w = root3over2;
+const double _h = 0.5;
 
 /// Convert from unit grid (Position)
 /// to unit screen coordinate space (Offset).
 /// Y is upwards in both coordinate spaces.
 /// We need unit space because
 /// it makes more sense than grid space because
-/// it isn't skewed, so
+/// it isn't skewed, so for example
 /// standard vector maths works.
+Offset positionToUnitOffset(Position position) =>
+    Offset(_w * position.x, _h * position.x - position.y);
 
 Offset unitOffsetToPositionOffset(Offset unitOffset) =>
-    Offset(unitOffset.dx / W, H * unitOffset.dx / W - unitOffset.dy);
-
-Offset positionToUnitOffset(Position position) =>
-    Offset(W * position.x, H * position.x - position.y);
+    Offset(unitOffset.dx / _w, _h * unitOffset.dx / _w - unitOffset.dy);
