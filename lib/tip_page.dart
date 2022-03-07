45bfd4757f874f6b9437a3bdfe429cca1ac46c64
paulsump <sumpner@hotmail.com>
2022-03-07 10:07:30 +0000

    added button to tipspageview

--- lib/tip_page.dart
+++ lib/tip_page.dart
@@ -1,7 +1,7 @@
-import 'package:cube_painter/alert.dart';
+import 'package:cube_painter/asset_icons.dart';
+import 'package:cube_painter/buttons/flat_hexagon_button.dart';
 import 'package:cube_painter/transform/screen_size.dart';
 import 'package:flutter/material.dart';
-
 
 const _emphasisStyle = TextStyle(
   fontWeight: FontWeight.bold,
@@ -48,12 +48,12 @@ void showHelp(BuildContext context) {
     builder: (BuildContext context) {
 //TODO replace this Alert with pageViews at the the start or when press help button
       return const TipsPageView();
-      return Alert(
-        title: 'Tips',
-        child: const _Tip(name: 'oneFinger'),
-        yesCallBack: () => Navigator.of(context).pop(),
-        yesTip: 'Done',
-      );
+      // return Alert(
+      //   title: 'Tips',
+      //   child: const _Tip(name: 'oneFinger'),
+      //   yesCallBack: () => Navigator.of(context).pop(),
+      //   yesTip: 'Done',
+      // );
     },
   );
 }
@@ -65,14 +65,37 @@ class TipsPageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        PageView(children: const [
-          TipPage(name: 'oneFinger'),
-          TipPage(name: 'oneFinger'),
-          TipPage(name: 'oneFinger'),
-        ]),
-      ],
+    return PageView(children: const [
+      TipPage(name: 'oneFinger'),
+      TipPage(name: 'oneFinger'),
+      TipPage(name: 'oneFinger'),
+    ]);
+  }
+}
+
+class _Button extends StatelessWidget {
+  final VoidCallback? onPressed;
+
+  final IconData icon;
+  final String tip;
+
+  const _Button({
+    Key? key,
+    this.onPressed,
+    required this.icon,
+    required this.tip,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: screenAdjust(0.017312, context)),
+      child: IconFlatHexagonButton(
+        icon: icon,
+        iconSize: screenAdjustAssetIconSize(context),
+        onPressed: onPressed,
+        tip: tip,
+      ),
     );
   }
 }
@@ -84,7 +107,18 @@ class TipPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _Tip(name: name);
+    return Column(
+      children: [
+        Expanded(child: _Tip(name: name)),
+        Expanded(
+          child: _Button(
+            onPressed: Navigator.of(context).pop,
+            icon: AssetIcons.thumbsUp,
+            tip: 'Close the tips.',
+          ),
+        ),
+      ],
+    );
   }
 }
 
