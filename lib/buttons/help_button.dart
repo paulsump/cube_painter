cbb92a11b2a072294e0de1a061a2a2d9eac0b2ec
paulsump <sumpner@hotmail.com>
2022-03-07 07:27:34 +0000

    displaying image on tip

--- lib/buttons/help_button.dart
+++ lib/buttons/help_button.dart
@@ -30,32 +30,26 @@ const _emphasisStyle = TextStyle(
   decoration: TextDecoration.underline,
 );
 
-const _oneFingerTip = <TextSpan>[
-  TextSpan(text: 'Add cubes', style: _titleStyle),
-  TextSpan(text: '...\n\nDrag with '),
-  TextSpan(text: 'one', style: _emphasisStyle),
-  TextSpan(text: ' finger.'),
-];
+const _tips = <String, List<TextSpan>>{
+  'oneFinger': <TextSpan>[
+    TextSpan(text: 'Add cubes', style: _titleStyle),
+    TextSpan(text: '...\n\nDrag with '),
+    TextSpan(text: 'one', style: _emphasisStyle),
+    TextSpan(text: ' finger.'),
+  ],
+  'twoFinger': <TextSpan>[
+    TextSpan(text: 'Pan and Zoom', style: _titleStyle),
+    TextSpan(text: '...\n\nDrag with '),
+    TextSpan(text: 'two', style: _emphasisStyle),
+    TextSpan(text: ' fingers.'),
+  ],
+  'longPress': <TextSpan>[
+    TextSpan(text: 'Button tips', style: _titleStyle),
+    TextSpan(text: '...\n\nPress and hold a button.'),
+  ],
+};
 
-const _twoFingerTip = <TextSpan>[
-  TextSpan(text: 'Pan and Zoom', style: _titleStyle),
-  TextSpan(text: '...\n\nDrag with '),
-  TextSpan(text: 'two', style: _emphasisStyle),
-  TextSpan(text: ' fingers.'),
-];
-
-const _longPressTip = <TextSpan>[
-  TextSpan(text: 'Button tips', style: _titleStyle),
-  TextSpan(text: '...\n\nPress and hold a button.'),
-];
-
-final _tips = <_Tip>[
-  const _Tip(textSpans: _oneFingerTip),
-  const _Tip(textSpans: _twoFingerTip),
-  const _Tip(textSpans: _longPressTip),
-];
-
-/// Show a little message to get them started.
+/// Show a few little message with an image to get them started.
 void _showHelp(BuildContext context) {
   showDialog(
     context: context,
@@ -63,8 +57,8 @@ void _showHelp(BuildContext context) {
       return Alert(
         title: 'Tips',
         // todo just display one tip at a time
-        //   final assets = await _Assets._getStrings('help/triangle_');
-        child: _tips[0],
+
+        child: const _Tip(name: 'oneFinger'),
         yesCallBack: () => Navigator.of(context).pop(),
         yesTip: 'Done',
       );
@@ -73,20 +67,28 @@ void _showHelp(BuildContext context) {
 }
 
 class _Tip extends StatelessWidget {
-  final List<TextSpan> textSpans;
+  final String name;
 
-  const _Tip({Key? key, required this.textSpans}) : super(key: key);
+  const _Tip({Key? key, required this.name}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return RichText(
-      text: TextSpan(
-        text: '\n',
-        style: DefaultTextStyle.of(context).style.apply(
-              fontSizeFactor: screenAdjust(0.004, context),
-            ),
-        children: textSpans,
-      ),
+    return Column(
+      children: [
+        Image(
+          // width: screenAdjust(),
+          image: AssetImage('images/$name.png'),
+        ),
+        RichText(
+          text: TextSpan(
+            text: '\n',
+            style: DefaultTextStyle.of(context).style.apply(
+                  fontSizeFactor: screenAdjust(0.004, context),
+                ),
+            children: _tips['name'],
+          ),
+        ),
+      ],
     );
   }
 }
